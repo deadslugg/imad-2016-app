6a56f2d2d238239a8ac342373741f6f7ae40bffc
deadslugg <sreejithkramachandran@gmail.com>
2016-09-30 08:46:31 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article one', function (req,res) {
+res.send('article one will be send shortly');
+});
 
+app.get('/article two', function (req,res) {
+res.send('article two will be send shortly');
+});
+
+app.get('/article three', function (req,res) {
+res.send('article three will be send shortly');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
