eda43911ffbbc4a632627c19a17b9345af87bfb4
deadslugg <sreejithkramachandran@gmail.com>
2016-10-01 03:39:48 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title:'Article One | Sreejith',
+    heading:'Article One',
+    date:'oct 1',
+    content:`<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>  
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p> `
+};
+function createtemplate (data) {
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+var htmltemplate = 
+`<html>
+<head>
+    <title> 
+    ${title}
+    </title>    
+<link href="/ui/style.css" rel="stylesheet" />    
+             </head>
+  
+<body>
+<div class="container"> 
+     <div>
+<a href="/">HOME </a>    
+</div>    
+<div>
+<hr/>    
+</div>
+<div>
+<h3>
+${heading}   
+</h3> 
+</div>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>    
+</body>
+</html>`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
