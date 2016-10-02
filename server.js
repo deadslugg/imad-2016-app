a97cf11444a979643c77de1a35417d838ab7e75f
deadslugg <sreejithkramachandran@gmail.com>
2016-10-02 19:17:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleone = {
+var articles = {
+articleone : {
     title:'Article One | Sreejith',
     heading:'Article One',
     date:'oct 1',
@@ -21,6 +21,37 @@ This is the content.view more.This is the content.view more.
 This is the content.view more.This is the content.view more.
 This is the content.view more.This is the content.view more.          
 </p> `
+},
+articletwo : {    title:'Article two | Sreejith',
+    heading:'Article two',
+    date:'oct 1',
+    content:`<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>  
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p> `},
+articlethree: {    title:'Article three | Sreejith',
+    heading:'Article three',
+    date:'oct 1',
+    content:`<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p>  
+<p>
+This is the content.view more.This is the content.view more.
+This is the content.view more.This is the content.view more.          
+</p> `}
 };
 function createtemplate (data) {
     var title=data.title;
