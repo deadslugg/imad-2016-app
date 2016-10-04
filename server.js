34df6388aabfc9233388f975fe41e84ed063e8ba
deadslugg <sreejithkramachandran@gmail.com>
2016-10-04 23:28:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,14 +97,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('/:articleName', function (req,res) {
 var articleName = req.params.articleName;    
   res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
